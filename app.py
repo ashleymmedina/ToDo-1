4c150c176023988296035beceb4c63b55ce02855
ashleymmedina <ashley.meza-medina@csedge.org>
2024-11-16 13:43:11 -0800

    part 2

--- app.py
+++ app.py
@@ -18,27 +18,40 @@ todos = Query()
 #    userinput == input("Enter your command: ")
  
 
-# def read():
-#     get.userdata 
+def create():
+    userinput == input("Enter your todo: ")
+    db.insert({'todo' : userinput})
+    print("Todo made successfully!!!")
+
+def read():
+    allData = db.all()
+    for data in allData:
+        print(f'Todo {data.doc_id} : {data["todo"]}')
+
 
 while True:
     print("1. Create")
     print("2. Read")
     print("3. Update")
     print("4. Delete")
-
     userinput = input("Enter your command: ")
 
-    if userinput == 'Create':
+    if userinput == 'Create' or '1':
         userinput == input("Enter the task you want to add: ")
+        create()
 
-    if userinput == 'Update':
-        userinput == input("Insert what you would you like to update: ")
-        update = db.update(("userinput"), userinput == '')
+    if userinput == 'Read' or '2':
+        read()
 
-    if userinput == 'Delete':
+    if userinput == 'Update' or '3':
+        userinput == input("Insert what you would you like to replace: ")
+        update = db.update(("todos"), userinput == '')
+        update()
+
+    if userinput == 'Delete' or '4':
         userinput == input("Insert what you would like to remove: ")
-        delete = db.remove(userinput == '')
+        delete = db.remove(todos.todo == userinput)
+        delete()
 
 
     
